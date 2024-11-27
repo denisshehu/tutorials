5aa1f6478c5521f4d7fb58654e3beb8ecc5062c6
denisshehu <itsdenisshehu@gmail.com>
2024-11-27 12:01:17 +0100

    Finished 3-react

---
+++ 3-react/dojo-blog/1-during-tutorial/30.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("denis");
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blog),
+      }).then(() => {
+        console.log("New blog added.");
+        setIsPending(false);
+        navigate(-1);
+      });
+    }, 1000);
+  };
+
+  return (
+    <div className="create">
+      <h2>Add a new blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="denis">Denis</option>
+          <option value="dea">Dea</option>
+          <option value="kiki">Kiki</option>
+        </select>
+        {!isPending && <button>Add blog</button>}
+        {isPending && <button disabled>Adding blog...</button>}
+      </form>
+    </div>
+  );
+};
+
+export default Create;
--- 3-react/dojo-blog/src/App.js
+++ 3-react/dojo-blog/src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Create from "./Create";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
